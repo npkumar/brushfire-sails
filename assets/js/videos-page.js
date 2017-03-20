7e79e8d72e2145ea933a5d1335f7545e3248f59f
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2017-03-20 12:54:05 +0800

    sails angular init commit

---
+++ assets/js/videos-page.js
@@ -0,0 +1,61 @@
+angular.module('brushfire_videosPage', []).config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist(['self', '*://www.youtube.com/**']);
+});
+
+angular.module('brushfire_videosPage').controller('PageCtrl', [
+  '$scope',
+  '$timeout',
+  function($scope, $timeout) {
+
+    $scope.videosLoading = true;
+
+    $timeout(function afterRetrievingVideos() {
+      var _videos = [
+        {
+          title: 'FUNNY BABY VIDEOS',
+          src: 'https://www.youtube.com/embed/_FvTVWjLiHM'
+        }, {
+          title: 'Justin Bieber - Baby ft. Ludacris',
+          src: 'https://www.youtube.com/embed/kffacxfA7G4'
+        }, {
+          title: 'Charlie bit my finger - again !',
+          src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
+        }
+      ];
+
+      $scope.videosLoading = false;
+      $scope.videos = _videos;
+    }, 750);
+
+    $scope.submitNewVideo = function() {
+      if ($scope.busySubmittingVideo) {
+        return;
+      }
+      var _newVideo = {
+        title: $scope.newVideoTitle,
+        src: $scope.newVideoSrc
+      };
+
+      var parser = document.createElement('a');
+
+      parser.href = _newVideo.src
+
+      var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1, parser.search.length);
+
+      _newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
+
+      $scope.busySubmittingVideo = true;
+
+      $timeout(function() {
+        $scope.videos.unshift(_newVideo);
+
+        $scope.busySubmittingVideo = false;
+
+        $scope.newVideoTitle = '';
+        $scope.newVideoSrc = '';
+
+      }, 750);
+    }
+    
+  }
+]);
