a59f00fadbf7625b1d4e954a4304ccb769bab7b9
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2017-03-23 14:34:13 +0800

    add signup route

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,38 @@
  */
 
 module.exports = {
-	
-};
 
+  signup: function(req, res) {
+
+    if (_.isUndefined(req.param('email'))) {
+      return res.badRequest('An email address is required!');
+    }
+
+    if (_.isUndefined(req.param('password'))) {
+      return res.badRequest('A password is required!');
+    }
+
+    if (req.param('password').length < 6) {
+      return res.badRequest('Password must be at least 6 characters!');
+    }
+
+    if (_.isUndefined(req.param('username'))) {
+      return res.badRequest('A username is required!');
+    }
+
+    if (req.param('username').length < 6) {
+      return res.badRequest('Username must be at least 6 characters!');
+    }
+
+    if (!_.isString(req.param('username')) || req.param('username').match(/[^a-z0-9]/i)) {
+      return res.badRequest('Invalid username: must consist of numbers and letters only.');
+    }
+
+    var options = {
+      email: req.param('email'),
+      username: req.param('username'),
+      password: req.param('password')
+    };
+    return res.json(options);
+  }
+};
